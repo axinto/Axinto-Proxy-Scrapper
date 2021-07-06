0ce49a61d360eea719b8c6221cc8913308372249
axinto <68763279+axinto@users.noreply.github.com>
2021-07-06 13:22:38 +0530

    Add files via upload

---
+++ Axinto proxy/Data/axinto/About.bat
@@ -0,0 +1,53 @@
+ÿþ
+@echo off
+mode 800
+color B
+CLS
+echo. 
+echo.                                      AAA                                     iiii                            tttt                           
+echo.                                     A:::A                                   i::::i                        ttt:::t                           
+echo.                                    A:::::A                                   iiii                         t:::::t                           
+echo.                                   A:::::::A                                                               t:::::t                           
+echo.                                  A:::::::::A           xxxxxxx      xxxxxxxiiiiiiinnnn  nnnnnnnn    ttttttt:::::ttttttt       ooooooooooo   
+echo.                                 A:::::A:::::A           x:::::x    x:::::x i:::::in:::nn::::::::nn  t:::::::::::::::::t     oo:::::::::::oo 
+echo.                                A:::::A A:::::A           x:::::x  x:::::x   i::::in::::::::::::::nn t:::::::::::::::::t    o:::::::::::::::o
+echo.                               A:::::A   A:::::A           x:::::xx:::::x    i::::inn:::::::::::::::ntttttt:::::::tttttt    o:::::ooooo:::::o
+echo.                              A:::::A     A:::::A           x::::::::::x     i::::i  n:::::nnnn:::::n      t:::::t          o::::o     o::::o
+echo.                             A:::::AAAAAAAAA:::::A           x::::::::x      i::::i  n::::n    n::::n      t:::::t          o::::o     o::::o
+echo.                            A:::::::::::::::::::::A          x::::::::x      i::::i  n::::n    n::::n      t:::::t          o::::o     o::::o
+echo.                           A:::::AAAAAAAAAAAAA:::::A        x::::::::::x     i::::i  n::::n    n::::n      t:::::t    tttttto::::o     o::::o
+echo.                          A:::::A             A:::::A      x:::::xx:::::x   i::::::i n::::n    n::::n      t::::::tttt:::::to:::::ooooo:::::o
+echo.                         A:::::A               A:::::A    x:::::x  x:::::x  i::::::i n::::n    n::::n      tt::::::::::::::to:::::::::::::::o
+echo.                        A:::::A                 A:::::A  x:::::x    x:::::x i::::::i n::::n    n::::n        tt:::::::::::tt oo:::::::::::oo 
+echo.                        AAAAAAA                   AAAAAAAxxxxxxx      xxxxxxxiiiiiiii nnnnnn    nnnnnn          ttttttttttt     ooooooooooo   
+echo.                                                                                                                         
+echo.                                                                                                                                                                                                                                                                                                                                                                                       
+echo.                                                                                                                                                                                                                                                                      
+echo.                                                                                                                                                                                                                                             
+echo.                                                                            
+echo.                                                                 Discord = Axinto#7777
+echo.  
+echo.   
+echo.                                                                 Server Link = https://discord.gg/w9wRhxPqqa 
+echo. 
+echo. 
+echo.                                                                 Vanity link = dsc.gg/axeed
+echo.
+echo. 
+echo. 
+echo. 
+echo.                                                                 If You Have Any Questions OR Issues Please Dm Me On Discord
+echo. 
+echo. 
+echo. 
+echo.  
+echo.                                                                 Enjoy
+echo.  
+SET BROWSER=firefox.exe
+SET WAIT_TIME=2
+START %BROWSER% -new-tab "https://www.youtube.com/channel/UC6Nx2EO3Ewj_Cv43pFPpiQQ"
+@ping 127.0.0.1 -n %WAIT_TIME% -w 1000 > nul
+START %BROWSER% -new-tab "https://youtu.be/Bo4luC4AHDY"
+pause
+taskkill /f /im About.bat
+                                                          
